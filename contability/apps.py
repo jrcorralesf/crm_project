e18c225dfadcd311c1a287e39eafdf236eee47c9
Julian Ricardo Corrales Fierro <jrcorralesf@unal.edu.co>
2021-06-10 09:04:49 -0500

    Crm project creation

---
+++ contability/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ContabilityConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'contability'
---
+++ crm_project/settings/local.py
@@ -0,0 +1,19 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+STATIC_URL = '/static/'
